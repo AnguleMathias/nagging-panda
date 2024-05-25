51e658b676d2df3809b075119bde6caba4536153
Angule Mathias <angulemathias3@gmail.com>
2024-05-25 23:17:04 +0300

    boiler plate commit, set up swagger docs and concurrently

---
+++ api/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription(
+      'Task management API that allows users to create, edit and delete tasks',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await app.listen(8080);
+}
+bootstrap();
